8e8dfe9ecb689ea6a887c40ee1febf2f24bc07fc
Jackson Wydra <jacksonwydra@gmail.com>
2020-04-01 16:49:02 -0400

    Added sass loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   }
